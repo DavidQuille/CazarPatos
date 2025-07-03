e33c452d6bd8c66c475c672f683f3fc49edd81bd
DESKTOP-OERS6GV <DESKTOP-OERS6GV@epn.edu.ec>
2025-07-03 15:37:20 -0500

    RankingActivity and Recycleview

---
+++ app/src/main/java/com/david/quille/cazarpatos/Player.kt
@@ -0,0 +1,5 @@
+package com.david.quille.cazarpatos
+
+data class Player (var username:String, var huntedDucks:Int){
+    constructor():this("",0)
+}
---
+++ app/src/main/java/com/david/quille/cazarpatos/RankingActivity.kt
@@ -0,0 +1,40 @@
+package com.david.quille.cazarpatos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class RankingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ranking)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+        var jugadores = arrayListOf<Player>()
+        jugadores.add(Player("David.Quille",10))
+        jugadores.add(Player("Jugador2",6))
+        jugadores.add(Player("Jugador3",3))
+        jugadores.add(Player("Jugador4",9))
+        jugadores.add(Player("Jugador5",1))
+        jugadores.add(Player("Jugador6",4))
+        jugadores.add(Player("Jugador7",2))
+        jugadores.add(Player("Jugador8",5))
+        jugadores.add(Player("Jugador9",8))
+        jugadores.add(Player("Jugador10",7))
+        jugadores.sortByDescending { it.huntedDucks }
+
+        val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
+        recyclerViewRanking.layoutManager = LinearLayoutManager(this);
+        recyclerViewRanking.adapter = RankingAdapter(jugadores);
+        recyclerViewRanking.setHasFixedSize(true);
+
+    }
+}
