a8a272daaa261f70d2f58ba1a6016db6bc2a5197
DESKTOP-OERS6GV <DESKTOP-OERS6GV@epn.edu.ec>
2025-07-18 15:01:02 -0500

    cretae db firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.security.crypto)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
